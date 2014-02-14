76e93090efb636762924d67d551f744b1fc48bc1
vagrant <vagrant@precise32.(none)>
2014-02-14 06:12:33 +0000

    lab6 commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, addProject);
+	console.log("projectHTML");
+	$(.details).click(addProjectDetails);
 }
 
 /*
--- routes/palette.js
+++ routes/palette.js
@@ -4,4 +4,12 @@ exports.randomPalette = function(req, res) {
 	// get a random palette from the top ones
 	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
 	res.send('Your random palette is called: ' + randomPalette['title']);
+	res.json(randomPalette);
+
 }
+
+$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
